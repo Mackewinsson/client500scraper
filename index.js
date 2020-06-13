37c660f950527d2bccc350b0c739e315f6d5880e
Mackewinsson <Mackewinsson@gmail.com>
2020-06-13 11:27:23 -0400

    Sraper working

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { Parser } = require("json2csv");
 const iconv = require("iconv-lite");
 const puppeteer = require("puppeteer");
 const { createWorker } = require("tesseract.js");
-const { data } = require("./resultsObject.json");
 require("events").EventEmitter.defaultMaxListeners = 500;
 
 // PARAMETROS DE BUSQUEDA
@@ -18,7 +17,7 @@ const tipo = ["comprar", "arrendar", "todos_los_avisos"];
 const palabraClave = "dueño";
 
 // THIS IS WHERE THE URL IS PLACED
-let URL = `https://www.yapo.cl/region_metropolitana/comprar?ca=15_s&l=0&q=due%C3%B1o&w=1&cmn=313&cmn=315&cmn=323&cmn=330&cmn=340&cmn=343`;
+let URL = `https://www.yapo.cl/region_metropolitana/comprar?ca=15_s&l=0&q=due%C3%B1o&f=p&w=1&cmn=305&cmn=307&cmn=310&cmn=313&cmn=315&cmn=316&cmn=319&cmn=323&cmn=330&cmn=334&cmn=335&cmn=336&cmn=340&cmn=343`;
 console.log(URL);
 // ARRAY FOR THE FIRST PAGE ITEMS
 let itemsUrlsArray = [];
@@ -82,12 +81,26 @@ let resultsObject = [];
   // } else {
   //   itemsNumber = parseInt(articleItems[0]);
   // }
-
-  let pages = 11;
-
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  let pages = 4;
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
+  // ====================================================== //
   // Math.ceil(itemsNumber / 50);
-  // POPULATE THE paginationUrlsArray
 
+  /* -------------------------------------------------------------------------- */
+  /*                      POPULATE THE paginationUrlsArray                      */
+  /* -------------------------------------------------------------------------- */
+
+  console.log(chalk.yellow(`1. Populating the pagination array`));
   for (let i = 0; i < pages; i++) {
     if (paginationUrlsArray.length == 0) {
       paginationUrlsArray.push(`${URL}`);
@@ -97,11 +110,13 @@ let resultsObject = [];
   }
   console.log(
     chalk.green(
-      `The paginationUrlsArray has: ${paginationUrlsArray.length} links`
+      `2. The paginationUrlsArray has: ${paginationUrlsArray.length} links`
     )
   );
 
-  // REQUEST TO EACH PAGINATION LINK
+  /* -------------------------------------------------------------------------- */
+  /*                     // REQUEST TO EACH PAGINATION LINK                     */
+  /* -------------------------------------------------------------------------- */
 
   for (let i = 0; i < paginationUrlsArray.length; i++) {
     response = await requestPromise({
@@ -137,16 +152,21 @@ let resultsObject = [];
       gzip: true,
     });
     $ = cheerio.load(response);
+    console.log(chalk.yellow(`3. Populating the items array`));
     element = $("td.thumbs_subject > a.title").each(function () {
       itemsUrlsArray.push($(this).attr("href"));
     });
     console.log(
-      chalk.green(`The itemsUrlsArray NOW has: ${itemsUrlsArray.length} links`)
+      chalk.green(
+        `4. The itemsUrlsArray NOW has: ${itemsUrlsArray.length} links`
+      )
     );
   }
   console.log(itemsUrlsArray);
 
-  // RESQUEST METHOD FOR THE EACH ARTICLE
+  /* -------------------------------------------------------------------------- */
+  /*                   // RESQUEST METHOD FOR THE EACH ARTICLE                  */
+  /* -------------------------------------------------------------------------- */
 
   for (let i = 0; i < itemsUrlsArray.length; i++) {
     response = await requestPromise({
@@ -189,9 +209,9 @@ let resultsObject = [];
     // HERE I LOAD THE RESPONSE AND GET THE URLS FOR MY ARRAYS
 
     $ = cheerio.load(response);
-
+    console.log(chalk.yellow(`5. Scraping item **${i}**`));
     // GETING THE ELEMENTS THAT I NEED FORM EACH ARTICLE
-
+    let id = i + 1;
     let title = $('h1[id="da_subject"]').text();
     let price = $('div[id="dataAd"]').attr("data-price");
     let seller = $("seller-info").attr("username");
@@ -210,9 +230,13 @@ let resultsObject = [];
       .text()
       .replace(/(?:\r\n|\r|\n)/g, "")
       .trim();
-    let link = itemsUrlsArray[i];
+    console.log(chalk.green(`6. Item **${i + 1}** Scraped SUCCESSFULLY`));
 
-    // PUPPETEER IMPLEMENTATION TO GET PHONE NUMBER WITH TESSERACT
+    /* -------------------------------------------------------------------------- */
+    /*       //  PUPPETEER IMPLEMENTATION TO GET PHONE NUMBER WITH TESSERACT      */
+    /* -------------------------------------------------------------------------- */
+
+    console.log(chalk.yellow(`7. Converting the phone's image`));
 
     const browser = await puppeteer.launch({
       // devtools: true,
@@ -246,13 +270,14 @@ let resultsObject = [];
     const {
       data: { text },
     } = await worker.recognize("./logo-screenshot.png");
-    // phone = text; text.replace(/(.....)(\s+)(\d{9}.?.?)/g, "(+56) $3");
-    console.log(text);
+    phone = text.replace(/(.....)(\s+)(\d{9}.?.?)/g, "(+56) $3");
+    console.log(phone);
     await worker.terminate();
-
+    console.log(chalk.green(`8. phone converted SUCCESSFULLY`));
     // I PUSH THE ELEMENTS TO AN JSON OBJECT
-
+    console.log(chalk.yellow(`9. Pushing elements to an object`));
     resultsObject.push({
+      id,
       title,
       price,
       seller,
@@ -263,34 +288,19 @@ let resultsObject = [];
       refcode,
       description,
     });
-    console.log(chalk.blue(`Item ${i + 1} Scraped...`));
-    // let stream = fetch(`https://www.yapo.cl${phoneUrl}`, {
-    //   headers: {
-    //     accept:
-    //       "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    //     "accept-encoding": "gzip, deflate, br",
-    //     "accept-language": "es-419,es;q=0.9",
-    //     "cache-control": "max-age=0",
-    //     referer:
-    //       "https://www.yapo.cl/region_metropolitana/celulares?ca=15_s&l=0&q=airpods&w=1&cmn=&ps=2&pe=4",
-    //     "sec-fetch-dest": "document",
-    //     "sec-fetch-mode": "navigate",
-    //     "sec-fetch-site": "same-origin",
-    //     "sec-fetch-user": "?1",
-    //     "upgrade-insecure-requests": "1",
-    //     "user-agent":
-    //       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-    //   },
-    // }).then((res) => {
-    //   const dest = fs.createWriteStream("./temp.gif");
-    //   res.body.pipe(dest);
-    // });
+    // CREATE JSONFILE
+    let data = JSON.stringify(resultsObject);
+    fs.writeFileSync("resultsObject.json", data);
+    console.log(chalk.green(`item SAVED TO JSON`));
   }
 
-  // HERE I CONVERT JSON TO CSV
+  /* -------------------------------------------------------------------------- */
+  /*                        // HERE I CONVERT JSON TO CSV                       */
+  /* -------------------------------------------------------------------------- */
 
-  console.log(chalk.blue("1. Starting JSON to CSV..."));
+  console.log(chalk.yellow("11. Starting JSON to CSV..."));
   const fields = [
+    "id",
     "title",
     "price",
     "seller",
@@ -300,7 +310,8 @@ let resultsObject = [];
     "timePublished",
     "refcode",
     "description",
-  ]; // I SPECIFY THE FIELDS THAT I NEED
+  ];
+  // I SPECIFY THE FIELDS THAT I NEED
   const json2csvParser = new Parser({
     fields: fields, // I SPECIFY THE FIELDS THAT I NEED
     // quote: "", // I ELIMINATE THE QUOTES FORM THE FIELDS
@@ -309,7 +320,7 @@ let resultsObject = [];
   });
 
   const csv = json2csvParser.parse(resultsObject);
-  fs.writeFileSync("./results.csv", csv, "utf-8");
-  console.log(chalk.blueBright("2. Done JSON to CSV..."));
-  //   nuevo
+  const ramdom = Math.floor(Math.random() * (1000000 - 100)) + 100;
+  fs.writeFileSync(`./results${ramdom}.csv`, csv, "utf-8");
+  console.log(chalk.blue("12. Done JSON to CSV..."));
 })();
