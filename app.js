b0fe262dbc122a084c08651f9e0940b8504e1c77
Mackewinsson <Mackewinsson@gmail.com>
2020-08-17 20:49:53 -0400

    fix in the port

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const yaposcraper = require("./yapo/yapo");
 const insertToDb = require("./db/insertToDb");
 const connectToMongoDb = require("./db/mongoConnection");
-socket = io.listen(process.env.PORT);
 
 const URL =
   "https://www.yapo.cl/region_metropolitana/inmuebles?ca=15_s&l=0&f=p&w=1&cmn=&st=a";
