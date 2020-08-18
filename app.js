deb8a9fef93f58f4444b35ac99fa3d5317189383
Mackewinsson <Mackewinsson@gmail.com>
2020-08-17 21:37:03 -0400

    trying to fix with procfile

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const yaposcraper = require("./yapo/yapo");
 const insertToDb = require("./db/insertToDb");
 const connectToMongoDb = require("./db/mongoConnection");
+const http = require("http");
 
 const URL =
   "https://www.yapo.cl/region_metropolitana/inmuebles?ca=15_s&l=0&f=p&w=1&cmn=&st=a";
+
+http.createServer(onRequest).listen(process.env.PORT || 6000);
 
 (async () => {
   try {
