14935b9be57574540bfe6d8e6cdb0c033b300d13
Mackewinsson <mackewinsson@gmail.com>
2020-10-09 08:59:51 -0300

    code added to jsoncsv

--- utils/jsonCsv.js
+++ utils/jsonCsv.js
@@ -2,21 +2,31 @@ const { Parser } = require('json2csv');
 
 const jsonToCsv = (data) =>{
 
-    const fields = ['field1', 'field2', 'field3'];
-    const opts = { fields };
-     
-    try {
-      const parser = new Parser(opts);
-      const csv = parser.parse(myData);
-      console.log(csv);
-    } catch (err) {
-      console.error(err);
-    }
+     // HERE I CONVERT JSON TO CSV
 
+  console.log("1. Starting JSON to CSV...");
+  const fields = [
+    "title",
+    "price",
+    "seller",
+    "phone",
+    "ispro",
+    "region",
+    "phoneUrl",
+    "timePublished",
+    "refcode",
+    "description",
+  ]; // I SPECIFY THE FIELDS THAT I NEED
+  const json2csvParser = new Parser({
+    fields: fields, // I SPECIFY THE FIELDS THAT I NEED
+    quote: "", // I ELIMINATE THE QUOTES FORM THE FIELDS
+    delimiter: '"', // I CHANGE THE DELIMITER FROM , WHICH IS DEFAULT TO "
+    defaultValue: "No info", // THIS IS THE DEFAULT VALUE WHEN THERE IS NO INFO IN THE FIELD
+  });
 
-
-
-
+  const csv = json2csvParser.parse(data);
+  fs.writeFileSync("./results.csv", csv, "utf-8");
+  console.log(chalk.blueBright("2. Done JSON to CSV..."));
 
 };
 
